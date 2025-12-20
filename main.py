958e473c7ba7e157baae942c57b67d847298fc82
nirdesho6o <n.bhandari2060@gmail.com>
2025-12-20 23:37:03 +0530

    progress

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Body
+from pydantic import BaseModel
+from typing import Optional
+
 
 app=FastAPI()
 @app.get("/")
-async def root():
+def root():
     return {"message": "Welcome to FastAPI!"}
 
-@app.get("/party")
-def party():
-    return {"message": "Let's party!"}
+
+my_posts = [{"title": "First Post", "content": "This is my first post"},
+            {"title": "Second Post", "content": "This is my second post"}]
+
+
+@app.get("/posts")
+def get_posts():
+    return {"data": my_posts}
+
+#pydantic model 
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+
+@app.post("/posts")
+def create_posts(new_post: Post):
+    print (new_post)
+    #to convert pydantic model to dictionary
+    print(new_post.model_dump())
+    return{"data":new_post}
