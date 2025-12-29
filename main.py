47f5b59b0cd03e97b8a3641f573cc17712071874
nirdesho6o <n.bhandari2060@gmail.com>
2025-12-29 23:22:20 +0530

    CRUD operations

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,Body
+from fastapi import FastAPI,Body,Response,status,HTTPException
 from pydantic import BaseModel
 from typing import Optional
 from random import randrange
@@ -35,9 +35,23 @@ def create_posts(new_post: Post):
     my_posts.append(post_dict)
     return{"data":post_dict}
 
-@app.get("/posts/{id}")
-def get_post(id: int):
+#IMPORTANT -this is writtten before posts/{id} so that it does not conflict with it
+@app.get("/posts/latest")   #to get the latest post
+def get_latest_post():
+    latest_post = my_posts[-1]
+    return {"latest_post": latest_post}
+
+
+@app.get("/posts/{id}")    #to get a specific post by id
+def get_post(id: int,response: Response):  #path parameter is int type
+    post = find_post(id)
+    if not post:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"message": "Post not found"}
+    return {"post_detail": post}
+
+#not the best way , but works for now
+def find_post(id):   #helper function to find a post by id
     for post in my_posts:
         if post['id'] == id:
-            return {"post_detail": post}
-    return {"message": "Post not found"}
+            return post
