da976f1d4740f8fc6c478224b4d062443d7d29b9
nirdesho6o <n.bhandari2060@gmail.com>
2026-01-07 15:37:57 +0530

    api

--- models.py
+++ models.py
@@ -1,9 +1,11 @@
-from pydantic import BaseModel
-
-class Employee(BaseModel):
-    id: int
-    name: str
-    age: int
-    department: str
-    is_full_time: bool = True  # default value
-    salary: float | None = None  # optional field
+from ast import pattern
+from pydantic import BaseModel,Field
+from typing import Optional
+class Employee(BaseModel):  
+    id: int=Field(...,gt=0)
+    name: str=Field(...,min_length=2,max_length=30)
+    age: int=Field(...,gt=18,lt=70)
+    department: str=Field(...,min_length=2,max_length=50)
+    is_full_time: bool = Field(default=True)  # default value
+    salary: Optional[float] = Field(default=None)  # optional field
+    email: Optional[str] = Field(default=None, pattern=r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')  # optional with regex validation
